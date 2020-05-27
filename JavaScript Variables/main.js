16443bf7d8815660e3f05dd9531f73d64de6faef
Mike Otharan <65412918+mikeotharan-code@users.noreply.github.com>
2020-05-26 22:47:14 -0300

    JavaScript Variables

---
+++ JavaScript Variables/main.js
@@ -0,0 +1,20 @@
+//Exercícios Curso ORIGAMID
+
+// Declarar uma variável com o seu nome
+var nome = 'Mike';
+
+// Declarar uma variável com sua idade
+var idade = 23;
+
+// Declarar uma variável com sua comida favorita
+// e não atribuir valor
+var comidaFavorita;
+
+// Atribuir valor asua comida favorita
+comidaFavorita = 'Pizza';
+
+// Declarar 5 variáveis diferentes sem valores
+var carro, contato, email, lingua, site;
+
+
+console.log(nome, idade, comidaFavorita);
